ac3cb837427ed7b7a1531c5c53f3f3ba8c80c2c4
Topias Vainio <toxip@disroot.org>
2019-06-03 07:58:41 +0000

    Travis #49

--- js/site.js
+++ js/site.js
@@ -51,3 +51,46 @@ function toggleMenu() {
     }
     console.log("dekimashita");
 };
+
+// for fixing timestamps to local time
+$(function () {    
+    $("time[data-format]").each(function () {
+        var el = $(this);
+        var dt = moment(el.attr("datetime"));
+        el.text(dt.format(el.data("format")));
+    });
+});
+
+function submitForm() {
+
+    var form = $('#comment-form');
+
+    $(form).addClass('disabled');
+    $('#comment-form-submit').html('<div class="lds-ring"><div></div><div></div><div></div><div></div></div> Loading...');
+
+    $.ajax({
+        type: "POST",
+        url: "https://dev.staticman.net/v3/entry/github/topiasv/toxip.net/master/comments",
+        data: $(form).serialize(),
+        contentType: 'application/x-www-form-urlencoded',
+        success: function (data) {
+            $('#comment-form-submit').html('Submit');
+            $('.post-comments-form .js-notice').removeClass('notice--danger').addClass('notice--success');
+            showAlert('<strong>Thank you for your comment!</strong> It will show on the site once it has been approved.');
+        },
+        error: function (err) {
+            console.log(err);
+            $('#comment-form-submit').html('Submit');
+            $('.post-comments-form .js-notice').removeClass('notice--success').addClass('notice--danger');
+            showAlert('<strong>Sorry, there was an error with your submission.</strong> Please make sure all required fields have been completed and try again.');
+            $(form).removeClass('disabled');
+        }
+    });
+
+    return false;
+};
+
+function showAlert(message) {
+    $('.post-comments-form .js-notice').removeClass('hidden');
+    $('.post-comments-form .js-notice-text').html(message);
+}
